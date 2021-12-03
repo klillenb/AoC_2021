4fc91b7e0b65124c2a07a17adb979098f9c558e2
klillenb <90192525+klillenb@users.noreply.github.com>
2021-12-03 15:18:59 +0200

    Delete 01.py

--- 01/01.py
+++
@@ -1,9 +0,0 @@
-file = open("01.input\input.txt", "r")
-data = [int(i) for i in file.readlines()]
-count_increase = 0
-
-for n1, n2 in zip(data, data[1:]):
-    if n1 < n2:
-        count_increase += 1
-            
-print(count_increase)
