163665ba42cceb39e951354075ccfd456c015153
klillenb <90192525+klillenb@users.noreply.github.com>
2021-12-03 15:30:03 +0200

    Day 2 part 1

---
+++ 02/02p1.py
@@ -0,0 +1,20 @@
+file = open("02.input\input.txt", "r")
+
+horizontal = 0
+depth = 0
+
+for line in file:
+    
+    if "forward" in line:
+        n = int(line.strip("forward \n"))
+        horizontal += n
+    
+    if "up" in line:
+        n = int(line.strip("up \n"))
+        depth -= n
+        
+    if "down" in line:
+        n = int(line.strip("down \n"))
+        depth += n
+
+print(f"{depth*horizontal}")
